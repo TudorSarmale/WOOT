cf8fd251e22b468fc17c9ae974904becd65b9f82
Voie Tudor <tudor_voie@yahoo.com>
2023-01-24 14:56:22 +0200

    Fix example.py

--- src/cogs/example.py
+++ src/cogs/example.py
@@ -2,14 +2,16 @@ import discord
 from discord.ext import commands
 import asyncio
 
+
 class Example(commands.Cog):
 
-	def __init__(self,bot):
-		self.bot = bot
+    def __init__(self, bot):
+        self.bot = bot
 
-	@commands.command()
-	async def test(self,ctx):
-		await ctx.reply("Test")
+    @commands.command()
+    async def test(self, ctx):
+        await ctx.reply("Test")
+
 
 async def setup(bot):
-	await bot.add_cog(Example(bot))
+    await bot.add_cog(Example(bot))
